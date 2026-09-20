795d0594d7cdf77ee0cd6f3105938166c54c66c6
BerryBGoode <fernandomena3131@gmail.com>
2026-09-19 23:32:57 -0600

    subiendo página (html y css)

---
+++ main.js
@@ -0,0 +1,8 @@
+//expresiones regulares
+/**
+ * Solo número ^[0-9]+$
+ * Solo letras ^[A-Za-z]+$
+ * Correo electronico ^[^\s@]+@[^\s@]+\.[^\s@]+$
+ * telefono ^[0-9]{4}-[0-9]{4}$
+ * numero de DUI /^[0-9]{8}[-][0-9]{1}$/
+*/
